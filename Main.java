c749edaadbd38607ce9615b6a3eca0ccce7880f5
rachelw106 <rachelw106@nycstudents.net>
2025-05-06 02:36:35 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main
 {
     public static void main(String[] args) {
-        
+        String[] wordNums = {"one", "two", "three", "four"};
+        WordPairList exampleOne = new WordPairList(wordNums);
+        System.out.println(exampleOne.getAllPairs());
+        String[] phrase = {"the", "more", "the", "merrier"};
+        WordPairList exampleTwo = new WordPairList(phrase);
+        System.out.println(exampleTwo.getAllPairs());
+        String[] moreWords = {"the", "red", "fox", "the", "red"};
+        WordPairList exampleThree = new WordPairList(moreWords);
+        System.out.println(exampleThree.numMatches());
     }
 }
